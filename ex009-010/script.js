6c54cad2c5be05b8b2d5dd31bf1676742fa61e51
Nesser Andrade <nesser24601@live.com>
2021-10-12 16:48:28 -0300

    tentando resolver o 9-10

--- ex009-010/script.js
+++ ex009-010/script.js
@@ -1,10 +1,33 @@
-document.querySelector("#botao").addEventListener("click", async () => {
-    resposta = await fetch(`https://api.chucknorris.io/jokes/random`)
+
+
+async function criarCategoria() {
+    resposta = await fetch(`https://api.chucknorris.io/jokes/categories`)
+    respostaJSON = await resposta.json()
+    seletor = document.querySelector("#seletor")
+    for (item of respostaJSON) {
+        let opcao = document.createElement("option")
+        let textoCategoria = document.createTextNode(item)
+        opcao.appendChild(textoCategoria)
+        opcao.setAttribute("value", item)
+        seletor.appendChild(opcao)
+    }
+}
+
+criarCategoria()
+
+let categoriaSelecionada = document.querySelector("#seletor")
+let selecao = categoriaSelecionada.querySelector('[selected]').value
+console.log(categoriaSelecionada)
+document.querySelector("#botao").addEventListener("click", () => criarPiada(categoriaSelecionada))
+
+async function criarPiada (categoria) {
+    resposta = await fetch(`https://api.chucknorris.io/jokes/random?category=${categoria}`)
     respostaJSON = await resposta.json()
     const { created_at: dataCriacao, id, url, icon_url: urlIcone, value: piada } = respostaJSON
     novaPiada = new Piada(dataCriacao, id, urlIcone, url, piada)
     campoPiada = document.querySelector("#piada")
     novaLI = document.createElement("li")
+    novaLI.setAttribute("data-category", categoria)
     campoImagem = document.createElement("img")
     campoImagem.setAttribute("src", novaPiada.urlIcone)
     novaLI.appendChild(campoImagem)
@@ -13,10 +36,7 @@ document.querySelector("#botao").addEventListener("click", async () => {
     novaLI.appendChild(data)
     novaLI.appendChild(texto)
     campoPiada.appendChild(novaLI)
-})
-
-
-
+}
 
 class Piada {
     constructor(dataCriacao, id, urlIcone, url, piada) {
